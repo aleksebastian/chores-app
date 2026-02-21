b9bc8fa1173e1b9707f30beb33d25414a37771b2
Alek Ortiz <aleksebastiano@gmail.com>
2026-02-21 09:56:38 -0300

    fix: add missing reference for webworker and declare ServiceWorkerGlobalScope

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,5 +1,8 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
 import { build, files, version } from '$service-worker';
+
+declare let self: ServiceWorkerGlobalScope;
 
 // Create a unique cache name for this deployment
 const CACHE = `cache-${version}`;
