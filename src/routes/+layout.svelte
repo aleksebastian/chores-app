dd555e0fbc75b4367513919fb7c40783770c3611
Alek Ortiz <aleksebastiano@gmail.com>
2026-02-21 09:48:50 -0300

    refactor: remove favicon import and link from layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { onNavigate } from '$app/navigation';
 
 	let { children } = $props();
@@ -19,5 +18,4 @@
 	});
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
